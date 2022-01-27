308214ae10255776fdcece54525525e1b7e5db5b
thatGui <gui@thatgui.com>
2022-01-27 15:21:37 +0000

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
+//
+const gameBoard = {
+  canvas: document.getElementById('canvas'),
+  ctx: canvas.getContext('2d'),
+  ccWidth: canvas.width,
+  cHeight: canvas.height,
+  road: Image.src = '/images/road.png',
+
+}
+//
+
+//
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    this.ctx = ctx;
+    
+    class Component {
+      constructor(){
+        this.width = cWidth;
+        this.height = cHeight;
+        imgObj.addEventListener('load', () => {
+          this.img = gameBoard.road;
+          
+      }
 };
