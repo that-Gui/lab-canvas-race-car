08a9c34a9322ec0f5243667a152f4591884d99f8
thatGui <gui@thatgui.com>
2022-01-27 17:52:25 +0000

    inital commit

--- js/index.js
+++ js/index.js
@@ -2,28 +2,64 @@
 const gameBoard = {
   canvas: document.getElementById('canvas'),
   ctx: canvas.getContext('2d'),
-  ccWidth: canvas.width,
+  cWidth: canvas.width,
   cHeight: canvas.height,
-  road: Image.src = '/images/road.png',
-
+  /* backGround: function () {
+    const img = new Image();
+    img.addEventListener('load', () => {
+        this.ctx.drawImage(img, 0, 0, this.cWidth, this.cHeight);
+    });
+    img.src ='/images/road.png';
+  }, */
+  bckgd(){
+    const img = new Image();
+    img.src ='/images/road.png';
+    img.addEventListener('load', () => {
+        this.ctx.drawImage(img, 0, 0, this.cWidth, this.cHeight);
+    });
+  }
 }
 //
+// class section
+class Car {
+  constructor() {
+    this.x = 250;
+    this.y = 600;
+    const img = new Image();
+    img.src ='/images/car.png';
+    img.addEventListener('load', () => {
+        gameBoard.ctx.drawImage(img, 250, 600, 88, 88);
+    });
+  }
+  moveRight() {
+    this.x += 25;
+  }
+  moveLeft() {
+    this.x -= 25;
+  }
+}
 
+//
 //
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
+}
+//
+//
   function startGame() {
-    this.ctx = ctx;
-    
-    class Component {
-      constructor(){
-        this.width = cWidth;
-        this.height = cHeight;
-        imgObj.addEventListener('load', () => {
-          this.img = gameBoard.road;
-          
-      }
-};
+    gameBoard.bckgd();
+    let car = new Car();
+}
+//
+window.addEventListener('keydown', (e) => {
+  switch (e.code) {
+    case 'ArrowRight':
+      car.moveRight();
+      break;
+    case 'ArrowLeft':
+      car.moveLeft();
+      break;
+  }
+})
